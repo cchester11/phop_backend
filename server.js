83db76c94557a52ad2cfd15eca7f19b6b48d77db
Charles <cchester1@angelo.edu>
2024-04-11 14:38:17 -0500

    server use assets folder static

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
 const cors = require('cors');
 
 app.use(cors());
+
+app.use('/photos', express.static(path.join(__dirname, 'assets')))
 
 app.listen(8000, (error) => {
       if(error) {
